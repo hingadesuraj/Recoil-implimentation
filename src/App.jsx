e390d30fe325aec0e7a45ad768cde6f5371ab672
Suraj <hingadesuraj@gmail.com>
2024-02-13 22:47:59 +0530

    use atom state variable in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { RecoilRoot, useRecoilValue } from "recoil";
-import { networkAtom } from "./atoms";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { jobsAtom, messagingAtom, networkAtom, notificaitonAtom } from "./atoms";
 
 function App() {
   
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <RecoilRoot>
-       <Main />
+       <Main /> 
      </RecoilRoot>
     </>
   );
@@ -15,13 +15,18 @@ function App() {
 
 function Main(){
   const networkNotificatoinCount = useRecoilValue(networkAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const messageCount = useRecoilValue(messagingAtom);
+  const NotificationCount = useRecoilValue(notificaitonAtom);
+  // const [messageCount,setMessageCount] = useRecoilState(messagingAtom);
+  
   return<>
    <div>
       <button>Home</button>
       <button>My Network ({networkNotificatoinCount >= 100 ? "99+": networkNotificatoinCount})</button>
-      <button>Jobs</button>
-      <button>Messaging</button>
-      <button>Notification</button>
+      <button>Jobs({jobsCount})</button>
+      <button>Messaging({messageCount})</button>
+      <button>Notification({NotificationCount})</button>
       <button>Me</button>
      </div>
   </>
