45b3b227aa173837ba50f5eba616e055870d68f0
Suraj <hingadesuraj@gmail.com>
2024-02-13 22:34:09 +0530

    all Atoms created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { networkAtom } from "./atoms";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <RecoilRoot>
+       <Main />
+     </RecoilRoot>
     </>
-  )
+  );
 }
 
-export default App
+function Main(){
+  const networkNotificatoinCount = useRecoilValue(networkAtom);
+  return<>
+   <div>
+      <button>Home</button>
+      <button>My Network ({networkNotificatoinCount >= 100 ? "99+": networkNotificatoinCount})</button>
+      <button>Jobs</button>
+      <button>Messaging</button>
+      <button>Notification</button>
+      <button>Me</button>
+     </div>
+  </>
+}
+
+export default App;
---
+++ src/atoms.js
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+
+export const networkAtom = atom({
+    key:"networkAtom",
+    default:108
+})
+
+export const jobsAtom = atom({
+    key:"jobsAtom",
+    default:0
+})
+
+export const notificaitonAtom = atom({
+    key:"notificaitonAtom",
+    default:12
+})
+
+export const messagingAtom = atom({
+    key:"messagingAtom",
+    default:0
+})
