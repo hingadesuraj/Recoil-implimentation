de1af13877af6bb08c8f56531ea521106b465404
hingadesuraj <hingadesuraj@gmail.com>
2024-02-14 14:45:09 +0530

    impliment updatetodo using atomfamily only update component render

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import "./App.css";
-import { RecoilRoot, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { todosAtomFamily } from "./atoms";
+import { useEffect } from "react";
  
 
 // fetch todo according to passed id
 function App() {
   return (
     <RecoilRoot>
+      <UpdateComponent/>
       <Todo id={1} />
       <Todo id={2} />
       <Todo id={2} />
       <Todo id={2} />
       <Todo id={1} />
       <Todo id={1} />
-     
     </RecoilRoot>
   );
+}
+
+
+function UpdateComponent(){
+  const updateTodo =  useSetRecoilState(todosAtomFamily(2));
+
+  useEffect(()=>{
+      setTimeout(()=>{
+        updateTodo({
+          id:"2",
+          title:"new Todo",
+          description:"new Todo"
+        })
+      },5000)
+  },[])
+  return <>
+    <div></div>
+  </>
 }
 
 function Todo({id}) {
