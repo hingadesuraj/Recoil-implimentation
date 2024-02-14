50a42bdd2ab0f1689085f64b98df6ee321cc9a2e
hingadesuraj <hingadesuraj@gmail.com>
2024-02-14 11:42:58 +0530

    4 atoms can be converted in single atom in atom.js and change selector acording to atom

--- src/atoms.js
+++ src/atoms.js
@@ -1,34 +1,58 @@
 import { atom, selector } from "recoil";
 
-export const networkAtom = atom({
-  key: "networkAtom",
-  default: 108,
-});
+// export const networkAtom = atom({
+//   key: "networkAtom",
+//   default: 108,
+// });
 
-export const jobsAtom = atom({
-  key: "jobsAtom",
-  default: 0,
-});
+// export const jobsAtom = atom({
+//   key: "jobsAtom",
+//   default: 0,
+// });
 
-export const notificaitonAtom = atom({
-  key: "notificaitonAtom",
-  default: 12,
-});
+// export const notificaitonAtom = atom({
+//   key: "notificaitonAtom",
+//   default: 12,
+// });
 
-export const messagingAtom = atom({
-  key: "messagingAtom",
-  default: 0,
-});
+// export const messagingAtom = atom({
+//   key: "messagingAtom",
+//   default: 0,
+// });
+
+
+// above all 4 atom consolidate in single atom below
+
+export const notificationCounter = atom({
+  key:"notificationCounter",
+  default:{
+    network:104,
+    jobs:0,
+    notifcation : 3,
+    message:0
+  }
+}) 
+
+
+// selector 
+export const totalNotificationSelector = selector({
+  key:"totalNotificationSelector",
+  get:({get})=>{
+    const notificationTotal = get(notificationCounter);
+    return notificationTotal.notifcation + notificationTotal.jobs + notificationTotal.message + notificationTotal.network ;
+  }
+})
+
 
 // selector define
-export const totalNotificationSelector = selector({
-  key: "totalNotificationSelector",
-  get: (props) => {
-    const networkCount = props.get(networkAtom);
-    const jobsCount = props.get(jobsAtom);
-    const notificationCount = props.get(notificaitonAtom);
-    const messageCount = props.get(messagingAtom);
+// export const totalNotificationSelector = selector({
+//   key: "totalNotificationSelector",
+//   get: (props) => {
+//     const networkCount = props.get(networkAtom);
+//     const jobsCount = props.get(jobsAtom);
+//     const notificationCount = props.get(notificaitonAtom);
+//     const messageCount = props.get(messagingAtom);
 
-    return networkCount + jobsCount + notificationCount + messageCount;
-  },
-});
+//     return networkCount + jobsCount + notificationCount + messageCount;
+//   },
+// });
